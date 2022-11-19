0f69c0e4c5207ec3d93b845fa125514c90c0b0cd
akaMiike <mike.nss@alu.ufc.br>
2022-11-19 20:37:23 -0300

    Adicionada tela de resultados

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app.secret_key = 'lances12345'
 
 lances = dict({})
 
-tempo_restante_rodada =99999
+tempo_restante_rodada = 10
+tempo_restante_retornar_inicio = 10
 
 premios = {
     "/static/carrinho.png" : "Carrinho da hotwheels 0 KM",
@@ -17,26 +18,28 @@ premios = {
 
 premio_rodada = []
 
-@app.route("/lances/maior-lance")
-def obter_maior_lance_unico():
-    global lances
-    lance_maximo_unico = -1
+@app.route("/timer/resultado")
+def timer_resultado():
+    global tempo_restante_retornar_inicio, tempo_restante_rodada, premio_rodada, lances
     
-    for key,value in lances.items():
-        if(len(value) == 1 and (lance_maximo_unico == -1 or key > lance_maximo_unico)):
-            lance_maximo_unico = key
+    tempo_restante_result = str(datetime.timedelta(seconds = (tempo_restante_retornar_inicio)))
+    
+    if(tempo_restante_retornar_inicio == 0):
+        tempo_restante_rodada = 10
+        tempo_restante_retornar_inicio = 10
+        premio_rodada = []
+        lances = {}
+    
+    else:
+        tempo_restante_retornar_inicio = tempo_restante_retornar_inicio - 1
 
-    return jsonify({"result": lance_maximo_unico})
+    return jsonify({"result": tempo_restante_result})
 
-@app.route("/timer")
-def timer():
+@app.route("/timer/rodada")
+def timer_rodada():
     global tempo_restante_rodada, premio_rodada
     
-    if(tempo_restante_rodada == 0):
-        tempo_restante_rodada = 9999
-        premio_rodada = []
-    
-    tempo_restante_result = str(datetime.timedelta(seconds = (tempo_restante_rodada-1)))
+    tempo_restante_result = str(datetime.timedelta(seconds = (tempo_restante_rodada)))
     tempo_restante_rodada = tempo_restante_rodada - 1
 
     return jsonify({"result": tempo_restante_result})
@@ -80,16 +83,31 @@ def fazer_lance():
     flash("Lance submetido com sucesso!")
     return redirect(url_for('tela_lances'))
     
-@app.route("/menor-lance")
-def obter_menor_lance_unico():
-    global lances
+@app.route("/lances/resultado")
+def obter_resultado():
+    global lances, premio_rodada, tempo_restante_rodada
+    
+    if(tempo_restante_rodada > 0):
+        return redirect(url_for('tela_lances'))
+    
     lance_minimo_unico = -1
+    nome_ganhador = ""
     
     for key,value in lances.items():
-        if(value == 1 and (lance_minimo_unico == -1 or key < lance_minimo_unico)):
+        if(len(value) == 1 and (lance_minimo_unico == -1 or key < lance_minimo_unico)):
             lance_minimo_unico = key
+            nome_ganhador = value[0]
                 
-    if(lance_minimo_unico != -1):
-        return f'<h1>O menor lance único foi: {lance_minimo_unico}</h1>'
-    else:
-        return '<h1>Não houve nenhum lance mínimo único ainda.</h1>'
+    return render_template('tela-resultado.html', nome = nome_ganhador, lance = lance_minimo_unico, premio=premio_rodada[0])
+
+
+@app.route("/lances/maior-lance")
+def obter_maior_lance_unico():
+    global lances
+    lance_maximo_unico = -1
+    
+    for key,value in lances.items():
+        if(len(value) == 1 and (lance_maximo_unico == -1 or key > lance_maximo_unico)):
+            lance_maximo_unico = key
+
+    return jsonify({"result": lance_maximo_unico})
